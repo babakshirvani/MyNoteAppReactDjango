3f2976b136bef78328a42582ba36f4f59ebcc512
Babak <babak@Babaks-MacBook-Pro.local>
2021-11-21 13:02:46 -0800

    add react dom Link and arrow icon

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as LeftArrow } from "../assets/leftArrow.svg";
 
-const NotePage = ({match}) => {
+const NotePage = ({ match }) => {
+  const noteID = match.params.id;
+  const [note, setNote] = useState(null);
 
-    const noteID = match.params.id;
-    
+  useEffect(() => {
+    getNote();
+  }, [noteID]);
 
-    return (
-        <div>
-            <h1>Single Note{noteID}</h1>
-        </div>
-    )
-}
+  const getNote = async () => {
+    const response = await fetch(`/api/notes/${noteID}`);
+    const data = await response.json();
+    setNote(data);
+  };
 
-export default NotePage
+  return (
+    <div className="note">
+      <div className="note-header">
+        <h3>
+          <Link to="/">
+            <LeftArrow />
+          </Link>
+        </h3>
+      </div>
+      <textarea defaultValue={note?.body}></textarea>
+    </div>
+  );
+};
+
+export default NotePage;
--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -1,29 +1,33 @@
-import React, {useState, useEffect} from 'react'
-import ListItem from '../components/ListItem';
+import React, { useState, useEffect } from "react";
+import ListItem from "../components/ListItem";
+
 const NotesListPage = () => {
+  const [notes, setNotes] = useState([]);
 
-    const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    getNotes();
+  }, []);
 
-    useEffect(() => {
-        getNotes()
-    }, [])
+  let getNotes = async () => {
+    const response = await fetch("/api/notes/");
+    const data = await response.json();
+    console.log("Data:::", data);
+    setNotes(data);
+  };
 
-    let getNotes = async () => {
-        const response = await fetch('/api/notes/')
-        const data = await response.json()
-        console.log("Data:::",data)
-        setNotes(data)
-    }
+  return (
+    <div className="notes">
+      <div className="notes-header">
+        <h2 className="notes-title">&#9782; Notes</h2>
+        <p className="notes-count">{notes.length}</p>
+      </div>
+      <div className="notes-list">
+        {notes.map((note, index) => (
+          <ListItem key={index} note={note} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <div className="notes-list">
-                {notes.map((note, index) => (
-                    <ListItem key={index} note={note} />
-                ))}
-            </div>
-        </div>
-    )
-}
-
-export default NotesListPage
+export default NotesListPage;
