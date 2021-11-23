8f0ce7b5c2f3b9d8b68a13ff7729ad1dc82b0c66
Babak <babak@Babaks-MacBook-Pro.local>
2021-11-22 18:03:04 -0800

    add summary of content under the title in note list

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const getTime = (note) => {
-  return new Date(note.updated).toLocaleDateString()
-}
+  return new Date(note.updated).toLocaleDateString();
+};
 
 let getTitle = (note) => {
   const title = note.body.split("\n")[0];
@@ -13,12 +13,26 @@ let getTitle = (note) => {
   return title;
 };
 
+const getContent = (note) => {
+  const title = getTitle(note);
+  let content = note.body.replaceAll('\n', ' ')
+  content = content.replaceAll(title, '')
+
+  if (content.length > 45){
+    return content.slice(0, 45) + '...'
+  } else {
+    return content
+  }
+};
+
 const ListItem = ({ note }) => {
   return (
     <Link to={`/notes/${note.id}`}>
       <div className="notes-list-item">
         <h3>{getTitle(note)}</h3>
-        <p><span>{getTime(note)}</span></p>
+        <p>
+          <span>{getTime(note)}</span>{getContent(note)}
+        </p>
       </div>
     </Link>
   );
