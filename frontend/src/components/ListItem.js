67084e3ea74a0524731332d36d2651434cea65de
Babak <babak@Babaks-MacBook-Pro.local>
2021-11-22 17:53:28 -0800

    display the first line of note 45chars in the list

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+let getTitle = (note) => {
+  const title = note.body.split("\n")[0];
+  if (title.length > 45) {
+    return title.splice(0, 45);
+  }
+  return title;
+};
+
 const ListItem = ({ note }) => {
   return (
     <Link to={`/notes/${note.id}`}>
       <div className="notes-list-item">
-        <h3>{note.body}</h3>
+        <h3>{getTitle(note)}</h3>
       </div>
     </Link>
   );
